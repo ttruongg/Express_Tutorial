951d8d775f90453bd271ce100e8b3d12d4697c9c
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-07-02 23:44:00 +0700

    router

---
+++ routes/users.js
@@ -0,0 +1,94 @@
+import { Router } from "express";
+import { mockUsers } from "../utils/constant.js";
+import {
+  query,
+  validationResult,
+  matchedData,
+  checkSchema,
+} from "express-validator";
+import { createUserValidationSchema } from "../utils/ValidationSchema.js";
+import { resolveIndexByUserId } from "../utils/middleware.js";
+const router = Router();
+
+// [GET] /api/users
+router.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-30 characters"),
+  (req, res) => {
+    const result = validationResult(req);
+    console.log(result);
+    const { filter, value } = req.query;
+
+    if (filter && value) {
+      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+    }
+
+    return res.send(mockUsers);
+  }
+);
+// [POST] /api/users
+// router.post(
+//   "/api/users",
+//   checkSchema(createUserValidationSchema),
+//   (req, res) => {
+//     const result = validationResult(req);
+
+//     if (!result.isEmpty()) {
+//       return res.status(400).send({ errors: result.array() });
+//     }
+
+//     const data = matchedData(req);
+
+//     // const { body } = req;
+//     const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data };
+//     mockUsers.push(newUser);
+//     return res.status(201).send(newUser);
+//   }
+// );
+
+// [PUT] /api/users/:id
+router.put("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { body, findUserIndex } = req;
+
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+  console.log(mockUsers);
+  return res.sendStatus(200);
+});
+// [PATCH] /api/users/:id
+router.patch("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { body, findUserIndex } = req;
+
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return res.sendStatus(200);
+});
+
+// [DELETE] /api/users/:id
+router.delete("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { findUserIndex } = req.params;
+
+  mockUsers.splice(findUserIndex, 1);
+  console.log(mockUsers);
+  return res.sendStatus(200);
+});
+
+//[GET] /api/users/:id
+router.get("/api/users/:id", (req, res) => {
+  console.log(req.params);
+  const parseId = parseInt(req.params.id);
+
+  if (isNaN(parseId)) res.status(400).send("Bad request. Invalid ID");
+
+  const user = mockUsers.find((user) => user.id === parseId);
+  if (!user) {
+    return res.status(404).send("User not found! ");
+  }
+
+  res.send(user);
+});
+
+export default router;
---
+++ utils/middleware.js
@@ -0,0 +1,18 @@
+export const resolveIndexByUserId = (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  const parseId = parseInt(id);
+  if (isNaN(parseId)) {
+    res.sendStatus(400);
+  }
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parseId);
+  if (findUserIndex === -1) return res.sendStatus(404);
+  req.findUserIndex = findUserIndex;
+  next();
+};
+
+export const loggingMiddleware = (req, res, next) => {
+  console.log(`${req.method} - ${req.url}`);
+  next();
+};
