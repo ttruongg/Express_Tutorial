f8e0c41fc15d4b45d31b241509ec55f1a650074a
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-07-08 23:51:12 +0700

    signedCookies and unsignedCookie

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
-
-import userRouter from "./routes/users.js";
-
+import cookieParser from "cookie-parser";
+import routes from "./routes/index.js";
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
-app.use(userRouter);
+app.use(cookieParser("helloworld"));
+app.use(routes);
+
 
 app.listen(PORT, () => {
   console.log("Server is running at port: " + PORT);
+});
+
+app.get("/", (req, res) => {
+  res.cookie("hello", "world", { maxAge: 10000, httpOnly: true, signed: true });
+  res.send({ msg: "Hello" });
 });
