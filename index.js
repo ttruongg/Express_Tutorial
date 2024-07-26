28f72a572adccfa1a3ca592746564cab920915ea
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-07-26 23:41:51 +0700

    authentic user, session pt2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { request } from "express";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import routes from "./src/routes/index.js";
+import { mockUsers } from "./src/utils/constant.js";
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -30,3 +31,25 @@ app.get("/", (req, res) => {
   res.cookie("hello", "world", { maxAge: 10000, httpOnly: true, signed: true });
   res.send({ msg: "Hello" });
 });
+
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password }
+  } = req;
+
+  const findUser = mockUsers.find((user) => user.username === username);
+
+  if (!findUser || findUser.password !== password) {
+    return res.status(401).send({ msg: "BAD CREDENTIALS" })
+  }
+
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not Authenticated" })
+})
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,25 +3,30 @@ export const mockUsers = [
     id: 1,
     username: "ray",
     displayName: "Ray",
+    password: "hello123"
   },
   {
     id: 2,
     username: "anson",
     displayName: "Anson",
+    password: "hello124"
   },
   {
     id: 3,
     username: "hawl",
     displayName: "Hawl",
+    password: "hellohello"
   },
   {
     id: 4,
     username: "henrry",
     displayName: "Henrry",
+    password: "test123"
   },
   {
     id: 5,
     username: "jacky",
     displayName: "Jacky",
+    password: "test456"
   },
 ];
