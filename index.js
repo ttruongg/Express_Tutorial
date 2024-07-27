bdb8cc0000d1f91a694fd2e44ff123d1036edca9
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-07-27 23:48:24 +0700

    virtual cart

--- index.js
+++ index.js
@@ -52,4 +52,23 @@ app.get("/api/auth/status", (req, res) => {
   return req.session.user
     ? res.status(200).send(req.session.user)
     : res.status(401).send({ msg: "Not Authenticated" })
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  const { body: item } = req;
+
+  const { cart } = req.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+
+});
+
+app.get("/api/cart/status", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  return res.send(req.session.cart ?? []);
 })
