4ce898ba3cba1e0b40d75a099f57391081d4e577
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-08-02 17:24:11 +0700

    passportJS pt2

--- src/strategies/local-strategy.js
+++ src/strategies/local-strategy.js
@@ -1,33 +1,29 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
 import { mockUsers } from "../utils/constant.js";
-
+import { User } from "../model/user.js";
 passport.serializeUser((user, done) => {
     console.log("Inside serializeUser");
     console.log(user);
     done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-    console.log("Inside deserializeUser");
-    console.log(`UserID: ${id}`);
+passport.deserializeUser(async (id, done) => {
     try {
-        const findUser = mockUsers.find((user) => user.id === id);
+        const findUser = await User.findById(id);
         if (!findUser) throw new Error("User not found");
-        done(null, id);
+        done(null, findUser);
     } catch (error) {
         done(error, null);
     }
 });
 
 export default passport.use(
-    new Strategy((username, password, done) => {
-        console.log(`Username: ${username}`);
-        console.log(`Password: ${password}`);
+    new Strategy(async (username, password, done) => {
         try {
-            const findUser = mockUsers.find((user) => user.username === username);
+            const findUser = await User.findOne({ username });
             if (!findUser) throw new Error("User not found");
-            if (findUser.password !== password) throw new Error("Invalid Credentials");
+            if (findUser.password !== password) throw new Error("Invalid Credential");
             done(null, findUser);
         } catch (error) {
             done(error, null);
